71b20f2387d8c1a6fc586d31967d66223766d5c4
Jeffrey Czarnowski <Jeff.Czarnowski@taxware.com>
2014-03-27 11:08:21 -0400

    Finished static pages

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -5,6 +5,11 @@ feature "Home page" do
     visit '/static_pages/home'
     expect(page).to have_text('Sample App')
   end
+
+  scenario "should have title 'Home'" do
+    visit '/static_pages/home'
+    expect(page).to have_title("Ruby on Rails Tutorials Sample App | Home")
+  end
 end
 
 feature "Help page" do
@@ -12,11 +17,21 @@ feature "Help page" do
     visit '/static_pages/help'
     expect(page).to have_text('Help')
   end
+
+  scenario "should have title 'Help'" do
+    visit '/static_pages/help'
+    expect(page).to have_title("Ruby on Rails Tutorials Sample App | Help")
+  end
 end
 
 feature "About page" do
   scenario "should have the content 'About'" do 
     visit '/static_pages/about'
     expect(page).to have_text('About')
+  end
+
+  scenario "should have title 'About'" do
+    visit '/static_pages/about'
+    expect(page).to have_title("Ruby on Rails Tutorials Sample App | About")
   end
 end
